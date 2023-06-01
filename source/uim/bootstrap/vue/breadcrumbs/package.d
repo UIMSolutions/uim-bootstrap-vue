2b3cd1575db84d150b06ceac77a282922a2066f6
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-01 07:25:24 +0200

    Update names

--- source/uim/bootstrap/vue/breadcrumbs/package.d
+++ source/uim/bootstrap/vue/breadcrumbs/package.d
@@ -1,7 +1,7 @@
 module uim.bootstrap.vue.breadcrumbs;
 
-public import uim.bootstrap.vue;
-
-public import uim.bootstrap.vue.breadcrumbs.breadcrumb;
-public import uim.bootstrap.vue.breadcrumbs.item;
-public import uim.bootstrap.vue.breadcrumbs.link;
+public {
+  import uim.bootstrap.vue.breadcrumbs.breadcrumb;
+  import uim.bootstrap.vue.breadcrumbs.item;
+  import uim.bootstrap.vue.breadcrumbs.link;
+}
